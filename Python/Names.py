feedbcf3707ef5983e01502a5311bf9862369497
brownae1331 <brownae1331@gmail.com>
2021-09-12 16:41:10 +0100

    Create Names.py

---
+++ Python/Names.py
@@ -0,0 +1,8 @@
+FirstName = ""
+LastName = ""
+print("Please enter your first and last name")
+print("First Name: ")
+FirstName = input()
+print("Last Name: ")
+LastName = input()
+print(FirstName, LastName)
