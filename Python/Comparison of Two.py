9521ff7b56681095300b9bfd17521628daf3ae6b
brownae1331 <brownae1331@gmail.com>
2021-09-12 16:41:03 +0100

    Create Comparison of Two.py

---
+++ Python/Comparison of Two.py
@@ -0,0 +1,8 @@
+print("Enter first number: ")
+num01 = int(input())
+print("enter second number: ")
+num02 = int(input())
+if num01 > num02:
+    print(num01, num02)
+elif num02 > num01:
+    print(num02, num01)
