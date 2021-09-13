5201631221b81d724db8311de30e3a85ce6e72df
brownae1331 <brownae1331@gmail.com>
2021-09-13 09:07:20 +0100

    Update Comparison of Three.py

--- Python/Comparison of Three.py
+++ Python/Comparison of Three.py
@@ -1,13 +1,31 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
 max = 0
-min = 10000
 mid = 0
-for i in range(3):
-    print("Enter number :")
-    number = int(input())
-    if number > max:
-        max = number
-    elif number < min:
-        min = number 
+min = 0
+if num1 > num2 and num1 > num3:
+    max = num1
+    if num2 > num3:
+        mid = num2
+        min = num3
     else:
-        mid = number
+        mid = num3
+        min = num2
+elif num2 > num1 and num2 > num3:
+    max = num2
+    if  num1 > num3:
+        mid = num1
+        min = num3
+    else:
+        mid = num3
+        min = num1
+elif num3 > num1 and num3 > num2:
+    max = num3
+    if num1 > num2:
+        mid = num1
+        min = num2
+    else:
+        mid = num2
+        min = num1
 print(min, mid, max)
