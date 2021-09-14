f8b2fee4d6b1c9d20ef20736b111ab5a6a6cfb81
brownae1331 <brownae1331@gmail.com>
2021-09-14 10:46:12 +0100

    Create Reverse.py

---
+++ Python/Reverse.py
@@ -0,0 +1,8 @@
+message = input()
+reverse = message[::-1]
+if reverse == message:
+    print(reverse)
+    print("This is a palindrome")
+else:
+    print(reverse)
+    print("This isn't a palindrome")
