92566df30c6803e84497aa5ea3ced16fc9706017
brownae1331 <brownae1331@gmail.com>
2021-09-14 11:57:26 +0100

    Update Comparison of Two.py

--- Python/Comparison of Two.py
+++ Python/Comparison of Two.py
@@ -2,7 +2,7 @@ print("Enter first number: ")
 num01 = int(input())
 print("enter second number: ")
 num02 = int(input())
-if num01 > num02:
+if num01 > num02:     #num 01 is the max
     print(num01, num02)
 elif num02 > num01:
-    print(num02, num01)
+    print(num02, num01) #num 02 is the max
