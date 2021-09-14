859726606550c6bb5c3bc01a1505f0adecb8e7c1
brownae1331 <brownae1331@gmail.com>
2021-09-14 11:57:24 +0100

    Update Caesar Cypher.py

--- Python/Caesar Cypher.py
+++ Python/Caesar Cypher.py
@@ -1,13 +1,13 @@
 message = input()
-for i in range(len(message)):
-    Ascii = ord(message[i])
-    if Ascii == 32:
+for i in range(len(message)):  
+    Ascii = ord(message[i])   #Turning each letter of the sentece into Ascii
+    if Ascii == 32:           #If the charater is a space
         print(chr(Ascii), end="")
-    elif Ascii == 121:
+    elif Ascii == 121:        #If the charater is 'y' 
         print(chr(97), end="")
-    elif Ascii == 122:
+    elif Ascii == 122:        #If the charater if 'x'
         print(chr(98), end="")
-    elif Ascii < 97 and Ascii > 64:
-        print(chr(Ascii + 34), end="")
+    elif Ascii < 97 and Ascii > 64: #If the character is a capital
+        print(chr(Ascii + 34), end="") 
     else: 
         print(chr(Ascii + 2), end="")
