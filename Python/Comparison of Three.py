618d68c82608d04c3d0aa063368253b1dff20426
brownae1331 <brownae1331@gmail.com>
2021-09-14 11:57:29 +0100

    Update Comparison of Three.py

--- Python/Comparison of Three.py
+++ Python/Comparison of Three.py
@@ -4,7 +4,7 @@ num3 = int(input())
 max = 0
 mid = 0
 min = 0
-if num1 > num2 and num1 > num3:
+if num1 > num2 and num1 > num3:   #If Num 1 is the max
     max = num1
     if num2 > num3:
         mid = num2
@@ -12,15 +12,15 @@ if num1 > num2 and num1 > num3:
     else:
         mid = num3
         min = num2
-elif num2 > num1 and num2 > num3:
+elif num2 > num1 and num2 > num3:   #If Num 2 is the max
     max = num2
     if  num1 > num3:
         mid = num1
         min = num3
     else:
         mid = num3
-        min = num1
-elif num3 > num1 and num3 > num2:
+        min = num1 
+elif num3 > num1 and num3 > num2:    #If Num 3 is the max
     max = num3
     if num1 > num2:
         mid = num1
