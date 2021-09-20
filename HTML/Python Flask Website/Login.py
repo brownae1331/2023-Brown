d05764c76e8edd03397d7dec68d0d5f265cc4e72
brownae1331 <brownae1331@gmail.com>
2021-09-20 13:11:56 +0100

    python flask

--- HTML/Python Flask Website/Login.py
+++ HTML/Python Flask Website/Login.py
@@ -1,6 +1,10 @@
-class Users:
-    def __init__(self, email, password, first_name, last_name,):
-        self.emial = email
+class Users():
+    def __init__(self, email, password, fname, lname):
+        self.email = email
         self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
+        self.first_name = fname
+        self.last_name = lname
+
+    def check_password(self):
+        if (any(x.isupper() for x in self.password)):
+            print("correct")
