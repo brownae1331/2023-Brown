ba69ded672bb45db27e2f83f0493f15395b49344
brownae1331 <brownae1331@gmail.com>
2021-09-20 10:49:27 +0100

    python flask

---
+++ HTML/Python Flask Website/Login.py
@@ -0,0 +1,6 @@
+class Users:
+    def __init__(self, email, password, first_name, last_name,):
+        self.emial = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
