97f6ed12db7ac5adcfebc177adc4671a9dcb1c8d
brownae1331 <brownae1331@gmail.com>
2021-09-21 14:31:24 +0100

    Adding css

--- HTML/Python Flask Website/Login.py
+++ HTML/Python Flask Website/Login.py
@@ -1,4 +1,6 @@
+import re
 class Users():
+
     def __init__(self, email, password, fname, lname):
         self.email = email
         self.password = password
@@ -6,5 +8,14 @@ class Users():
         self.last_name = lname
 
     def check_password(self):
-        if (any(x.isupper() for x in self.password)):
-            print("correct")
+        if (any(x.isupper() for x in self.password) and len(self.password) >= 6 and len(self.password) <= 12):
+            return True
+        else:
+            return False
+    
+    def check_email(self):
+        email_format = re.compile(r"[^@]+@[^@]+\.[^@]+")
+        if email_format.match(self.email):
+            return True
+        else:
+            return False
