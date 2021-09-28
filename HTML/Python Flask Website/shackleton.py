a3c4dd3b6434f9d05f49fd8a99485ba482bac624
brownae1331 <brownae1331@gmail.com>
2021-09-28 10:45:12 +0100

    More bootstrap and Python Flask

---
+++ HTML/Python Flask Website/shackleton.py
@@ -0,0 +1,17 @@
+from flask import Flask, app, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/endurance')
+def endurance():
+    return render_template('endurance.html')
+
+@app.route('/james_caird')
+def james_caird():
+    return render_template('james_caird.html')
+
+if __name__ == '__main__':
+    app.run(DEBUG = True)
