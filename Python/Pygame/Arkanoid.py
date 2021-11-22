549313f3e625e84e21363325447f433b45c0d29f
brownae1331 <brownae1331@gmail.com>
2021-11-22 15:39:36 +0000

    Update Arkanoid.py

--- Python/Pygame/Arkanoid.py
+++ Python/Pygame/Arkanoid.py
@@ -1,11 +1,16 @@
 import pygame
-
+import random
 
 # Define some colors
 BLACK = (0,   0,   0)
 WHITE = (255, 255, 255)
 RED = (255,   0,   0)
 BLUE = (0, 0, 255)
+GREY = (211, 211, 211)
+YELLOW = (255, 255, 0)
+MAGENTA = (255, 0, 255)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
 
 
 class Block(pygame.sprite.Sprite):
@@ -25,16 +30,20 @@ screen = pygame.display.set_mode([screenWidth, screenHeight])
 blockList = pygame.sprite.Group()
 allSpritesList = pygame.sprite.Group()
 
-for i in range(3):
+for i in range(6):
     if i == 0:
-        color = BLACK
-        y = 0
+        color = GREY
     elif i == 1:
         color = RED
-        y = 17
     elif i == 2:
-        color = BLUE
-        y = 34
+        color = YELLOW
+    elif i == 3:
+        color = CYAN
+    elif i == 4:
+        color = MAGENTA
+    elif i == 5:
+        color = GREEN
+    y = 17 * i
     for ii in range(15):
         block = Block(color, 45, 15)
         block.rect.x = (ii * 47)
@@ -68,15 +77,15 @@ while not done:
 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                padelSpeed = 3
+                padelSpeed = 5
             elif event.key == pygame.K_LEFT:
-                padelSpeed = -3
+                padelSpeed = -5
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                 padelSpeed = 0
 
-    screen.fill(WHITE)
+    screen.fill(BLUE)
 
     if paddle.rect.x >= 650:
         padelX -= 3
@@ -103,13 +112,20 @@ while not done:
     if pygame.sprite.collide_rect(ball, paddle) == True:
         yoffset *= -1
 
-    blockHitList = pygame.sprite.spritecollide(paddle, blockList, True)
+    # When ball hits block
+    blockHitList = pygame.sprite.spritecollide(ball, blockList, True)
+
+    powerUpX = ball.rect.x
+    powerUpY = ball.rect.y
 
     for block in blockHitList:
         yoffset *= -1
-
-    # When ball hits block
-    blockHitList = pygame.sprite.spritecollide(paddle, blockList, True)
+        rnd = random.randint(1, 10)
+        if rnd == 10:
+            powerUp = Block(YELLOW, 10, 10)
+            allSpritesList.add(powerUp)
+            powerUp.rect.x = powerUpX
+            powerUp.rect.y = powerUpY
 
     allSpritesList.draw(screen)
 
